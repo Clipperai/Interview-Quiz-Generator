5bff2d15778628fd9ffd08105ae92034ce6ffe30
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-09-19 21:10:46 +0530

    Revamp quiz generator with new functionality
    Refactored quiz generation logic and updated UI components.

--- quiz_generator.py
+++ quiz_generator.py
@@ -1,37 +1,47 @@
-import streamlit as st # type: ignore
-from brain import ask_ai
-
-st.title("Interview Quiz Generator")
-st.markdown("Sharpen your placement prep with real interview questions")
-
-#  Customization
-with st.form('My form'):
-    st.header("Customize Quiz")
-
-    role = st.selectbox("Select Role", ['Frontend', 'Backend', 'Full Stack', 'Core CS'])
-    difficulty = st.selectbox("Difficulty", ['Easy','Medium', 'Hard'])
-    topic = st.selectbox("Select:", ['Python', 'OOPs', 'OS', 'DBMS', 'DSA', 'CN', 'Aptitude'])
-    
-
-    start = st.form_submit_button("Start Quiz")        
-
-    if start:
-        with st.spinner("Generating..."):
-            prompt = f"""
-    Generate 5 {difficulty} level {topic} questions for {role} interviews.
-    
-        Format:
-        Q1: \n
-        Options:\n
-        A)
-        B)
-        C)
-        D) \n
-        Answer: \n
-        Explanation:
-        """
-            response = ask_ai(prompt)
-            st.write(response)
-    
+import streamlit as st
+from brain import generate_quiz, analyze_ai
 
 
+st.title("Interview Quiz Generator AI")
+st.caption("-v.1.1")
+
+
+# 1. Generate quiz only when the button is clicked
+if st.button("Generate Quiz"):
+
+    quiz_data = generate_quiz()
+
+    st.session_state.quiz = quiz_data["quiz"]
+
+
+# 2. Show quiz if it already exists
+if "quiz" in st.session_state:
+
+    quiz = st.session_state.quiz
+
+    with st.form("quiz_form"):
+
+        answers = {}
+
+        for i, question in enumerate(quiz):
+
+            st.subheader(f"Question {i + 1}")
+
+            answers[i+1] = st.radio(
+                question["question"],
+                question["options"],
+                key=f"question_{i}",
+                index=None
+            )
+
+        submitted = st.form_submit_button("Submit")
+
+        if submitted:
+            st.write(answers)
+
+st.download_button(
+    label="Download Text File",
+    data= str(quiz),
+    file_name="answers.text",
+    mime="text/plain"
+)
